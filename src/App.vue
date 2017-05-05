49ae4addb112dc82775a4d1e4f24c746e047a8bd
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-05-05 19:52:03 +0200

    todosVue INIT; Iniciem la pràctica todosVue, client Vue del todosBackend.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    Hola
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+</style>
