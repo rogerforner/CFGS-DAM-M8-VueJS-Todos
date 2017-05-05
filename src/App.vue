2a196994627977688d389f52c1a39eb440ce9448
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-05-05 20:18:34 +0200

    Afegim el toolbar i arreglem el layout del component App.

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,17 @@
        <md-icon>menu</md-icon>
       </md-button>
 
-      <h2 class="md-title">My App</h2>
+      <h2 class="md-title">todosVue</h2>
     </md-toolbar>
-
-    <div>
-      <md-button class="md-raised md-accent" @click="toggleRightSidenav">Toggle right</md-button>
-      <p>Open console to see the events</p>
-    </div>
 
     <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
       <md-toolbar class="md-large">
         <div class="md-toolbar-container">
-          <h3 class="md-title">Sidenav content</h3>
+          <h3 class="md-title">todosVue</h3>
         </div>
       </md-toolbar>
 
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi cupiditate esse necessitatibus beatae nobis, deserunt ut est fugit, tempora deleniti, eligendi commodi doloribus. Nemo, assumenda possimus, impedit inventore perferendis iusto!</p>
-    </md-sidenav>
-
-    <md-sidenav class="md-right" ref="rightSidenav" @open="open('Right')" @close="close('Right')">
-      <md-toolbar>
-        <div class="md-toolbar-container">
-          <h3 class="md-title">Sidenav content</h3>
-        </div>
-      </md-toolbar>
-
-      <md-button class="md-raised md-accent" @click="closeRightSidenav">Close</md-button>
     </md-sidenav>
   </div>
 </template>
@@ -42,6 +27,17 @@ export default {
   name: 'app',
   components: {
     Hello
+  },
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+    open (ref) {
+      console.log('Opened: ' + ref)
+    },
+    close (ref) {
+      console.log('Closed: ' + ref)
+    }
   }
 }
 </script>
