65fc4a8201364351df60206fab3b8a89e52a6adb
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-05-10 17:08:00 +0200

    Passport client fet

--- src/todosVue.js
+++ src/todosVue.js
@@ -1,11 +1,11 @@
 const todosVue = {
   STORAGE_KEY: 'todosvue_token',
-  AUTH_CLIENT_ID: '1',
+  AUTH_CLIENT_ID: '2',
   // AUTH_REDIRECT_URI: 'http://localhost:8095/login',
   AUTH_REDIRECT_URI: 'http://todosvue.rogerforne.2dam.acacha.org/login',
-  API_PROFILE_URL: 'http://todosbackend2.rogerforne.2dam.acacha.org:8080/api/v1/loguser',
+  API_PROFILE_URL: 'http://todosbackend2.rogerforne.2dam.acacha.org:8080/api/v1/user',
   ANDROID_SENDER_ID: '1066055111450',
-  REGISTER_GCM_TOKEN_URL: 'http://todosbackend2.rogerforne.2dam.acacha.org:8080/api/v1/user/gcmtoken',
-  GET_MESSAGES_URL: 'http://todosbackend2.rogerforne.2dam.acacha.org:8080/api/user/messages'
+  REGISTER_GCM_TOKEN_URL: 'http://todosbackend2.rogerforne.2dam.acacha.org:8080/api/v1/gcmtoken',
+  GET_MESSAGES_URL: 'http://todosbackend2.rogerforne.2dam.acacha.org:8080/api/v1/messages'
 }
 export default todosVue
